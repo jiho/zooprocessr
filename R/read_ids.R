6a79428cab6bd508b56f141668f991a3d8e95c30
Jean-Olivier Irisson <irisson@normalesup.org>
2014-10-02 13:35:54 +0200

    New version of read_ids and read_variables which uses the metadata table when available

--- R/read_ids.R
+++ R/read_ids.R
@@ -1,49 +1,65 @@
 #' Read the identifications of all objects in a project
 #'
 #' @inheritParams read_variables
-#' @param last when TRUE, only output the prediction and the last identification, called "Valid"; when FALSE, output all identifications (but still duplicate the last one and label it "Valid")
+#' @param last when TRUE, only output the prediction and the last validation, called "Valid"; when FALSE, output all validations (but still duplicate the last one and label it "Valid")
 #'
 #' @family project data handling functions
 #' @export
-#' @importFrom stringr str_c str_detect
+#' @importFrom stringr str_c str_detect str_replace
 #' @importFrom plyr ldply
-read_ids <- function(project, last=TRUE, verbose=TRUE, ...) {
-  # find dat1.txt files with the identifications
-  files <- list.files(str_c(project, "/PID_process/Pid_results/Dat1_validated"), pattern="dat1\\.txt$", full.names=TRUE)
+read_ids <- function(project, last=TRUE, ...) {
 
-  # determine wether to show a progress bar
-  if ( verbose & length(files) > 5 ) {
-    progress <- "text"
-  } else {
-    progress <- "none"
-  }
+  meta <- tryCatch(
+    # get metadata table if it exists
+    read_meta(project),
+    # if it does not exist, reconstruct it from the headers in the dat1.txt files
+    error=function(e) {
+      warning(e$message, ". Falling back on extracting data from *_dat1.txt files", call.=FALSE)
 
+      # list all dat1.txt files
+      files <- list.files(str_c(project, "/PID_process/Pid_results/Dat1_validated"), pattern="dat1\\.txt$", full.names=TRUE)
+      # extract the required metadata from them
+      l <- llply(files, read_pid, metadata=TRUE, data=FALSE)
+      m <- ldply(l, function(x) { c(fracnb=x$subsample$subpart, vol=x$sample$vol) })
+      # add scanid
+      scanid <- basename(files)
+      scanid <- str_replace(scanid, fixed("_dat1.txt"), "")
+      m$scanid <- scanid
 
-  # read all files
-  D <- ldply(files, function(f, ...) {
-
-    # read dat1 file
-    p <- read_pid(f, metadata=T, data=T, ...)
-    # extract meta data before modifying p
-    meta <- attr(p, "meta")
-
-    # keep validation columns
-    keep <- c("Item", "Label")
-    if ( last == TRUE ) {
-      keep <- c(keep, "Pred", "Valid")
-    } else {
-      keep <- c(keep, "Pres", names(p)[str_detect(names(p), "pred_valid_Id")], "Valid")
+      return(m)
     }
-    p <- p[,keep]
-    
-    # compute concentrations
-    sub <- meta$subsample$subpart
-    volume <- meta$sample$vol
+  )
+  
+  # read all files
+  D <- adply(meta, 1, function(m, ...) {
+    # construct file name
+    f <- str_c(project, "/PID_process/Pid_results/Dat1_validated/", m$scanid, "_dat1.txt")
 
-    p$concentration <- 1 / volume * sub
+    if ( ! file.exists(f) ) {
+      # warn if it does not exist and skip it
+      warning(m$scanid, " is in the metadata table but has no corresponding *_dat1.txt file")
+      p <- NULL
+    } else {
+      # read the data
+      p <- read_pid(f, metadata=FALSE, data=TRUE, ...)
+
+      # keep validation columns
+      keep <- c("Item", "Label")
+      if ( last ) {
+        keep <- c(keep, "Pred", "Valid")
+      } else {
+        keep <- c(keep, "Pres", names(p)[str_detect(names(p), "pred_valid_Id")], "Valid")
+      }
+      p <- p[,keep]
+    
+      # compute concentrations
+      p$concentration <- 1 / m$vol * m$fracnb
+    }
 
     return(p)
-  }, ..., .progress=progress)
+  }, ..., .progress=progress(files), .expand=FALSE)
+  # remove the index column
+  D <- D[,-1]
   
   return(D)
 }
--- R/read_variables.R
+++ R/read_variables.R
@@ -1,30 +1,45 @@
 #' Read the descriptive variables of all objects in a project
 #'
 #' @param project path to the project directory
-#' @param verbose when TRUE, show read progress (when there are many files to read)
 #' @param ... passed to \code{\link{read_pid}}
 #'
 #' @family project data handling functions
 #' @export
 #' @importFrom stringr str_c
 #' @importFrom plyr ldply
-read_variables <- function(project, verbose=TRUE, ...) {
-  # find pid files
-  files <- list.files(str_c(project, "/PID_process/Pid_results/Pid_predicted"), pattern="pid$", full.names=TRUE)
+read_variables <- function(project, ...) {
 
-  # determine wether to show a progress bar
-  if ( verbose & length(files) > 5 ) {
-    progress <- "text"
-  } else {
-    progress <- "none"
-  }
+  # get metadata table if it exists
+  meta <- tryCatch(
+    # get metadata table if it exists
+    read_meta(project),
+
+    error=function(e) {
+      warning(e$message, ". Falling back on listing *_dat1.pid files", call.=FALSE)
+      files <- list.files(str_c(project, "/PID_process/Pid_results/Pid_predicted"), pattern="pid$")
+
+      # extract scanid
+      scanid <- str_replace(scanid, fixed("_dat1.pid"), "")
+      m <- data.frame(scanid)
+
+      return(m)
+    }
+  )
 
   # read all files
-  D <- ldply(files, function(f, ...) {
-    # read pid file
-    p <- read_pid(f, data=T, ...)
+  D <- ldply(meta$scanid, function(id, ...) {
+    # construct file name
+    f <- str_c(project, "/PID_process/Pid_results/Pid_predicted/", id, "_dat1.pid")
+
+    if ( ! file.exists(f) ) {
+      # warn if it does not exist and skip it
+      warning(id, " is in the metadata table but has no corresponding *_dat1.pid file")
+      p <- NULL
+    } else {
+      p <- read_pid(f, data=TRUE, ...)
+    }
     return(p)
-  }, ..., .progress=progress)
+  }, ..., .progress=progress(files))
 
   return(D)
 }

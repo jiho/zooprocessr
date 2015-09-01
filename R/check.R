7bdd4cc23620e656aa7d86dc5d3e9b4bc96a5ad8
Jean-Olivier Irisson <irisson@normalesup.org>
2015-09-01 14:49:52 +0200

    Source dat1 files in different locations when checking
    And adapt error messages when necessary

--- R/check.R
+++ R/check.R
@@ -7,15 +7,15 @@
 #' This function
 #' \enumerate{
 #'   \item Reads the metadata table of the project
-#'   \item Lists and reads all \code{.pid} files in the \code{work} directory
-#'   \item Lists and reads all \code{dat1.tx} files in the \code{PID_process/Pid_results/Dat1_validated} directory
+#'   \item Lists and reads all \code{dat1.pid} files in the \code{work} directory
+#'   \item Lists and reads all \code{dat1.tx} files in \code{PID_process/Pid_results/Dat1_validated} (or \code{Dat1_extracted} if the previous one is empty)
 #'   \item Lists all images in the \code{PID_process/Sorted_vignettes} directory
 #' }
 #' Then it
 #' \enumerate{
-#'   \item Compares the list of samples in the metadata, \code{.pid}, and \code{.txt} files
-#'   \item Compares the list of objects in the \code{.pid}, \code{.txt} files, and present as images (i.e. vignettes)
-#'   \item For images present both in the \code{.txt} files and as vignettes, checks that the location of a vignette (its classification) matches the last column in the \code{txt} file
+#'   \item Compares the list of samples in the metadata, \code{dat1.pid}, and \code{dat1.txt} files
+#'   \item Compares the list of objects in the \code{dat1.pid}, \code{dat1.txt} files, and present as images (i.e. vignettes)
+#'   \item For images present both in the \code{dat1.txt} files and as vignettes, checks that the location of a vignette (its classification) matches the last column in the \code{dat1.txt} file
 #' }
 #'
 #' @return
@@ -42,6 +42,10 @@ check_project_dispatcher.zooscan <- function(project) {
   meta <- read_meta(project)
   pid_files <- list.files(str_c(project, "/Zooscan_scan/_work"), pattern=glob2rx("*.pid"), full=TRUE, recursive=TRUE)
   dat1_files <- list.files(str_c(project, "/PID_process/Pid_results/Dat1_validated"), pattern=glob2rx("*_dat1.txt"), full=TRUE)
+  if (length(dat1_files) == 0) {
+    dat1_files <- list.files(str_c(project, "/PID_process/Pid_results/Dat1_extracted"), pattern=glob2rx("*_dat1.txt"), full=TRUE)
+    dat1_files <- dat1_files[!str_detect(dat1_files, fixed("/Analysis_"))]
+  }
   
   # get sample identifiers from names
   pids <- str_replace(basename(pid_files), "_dat1.pid", "")
@@ -51,27 +55,27 @@ check_project_dispatcher.zooscan <- function(project) {
   attr(in_meta_no_pid, "message") <- "Sample in meta but no .pid file"
   
   in_meta_no_dat1 <- setdiff(meta$id, dat1)
-  attr(in_meta_no_dat1, "message") <- "Sample in meta but not in Dat1_validated"
+  attr(in_meta_no_dat1, "message") <- "Sample in meta but no .txt file"
 
   pid_not_in_meta <- setdiff(pids, meta$id)
   attr(pid_not_in_meta, "message") <- "Sample with .pid file but not in meta"
 
   dat1_not_in_meta <- setdiff(dat1, meta$id)
-  attr(dat1_not_in_meta, "message") <- "Sample in Dat1_validated but not in meta"
+  attr(dat1_not_in_meta, "message") <- "Sample with .pid file but not in meta"
   
   pid_no_dat1 <- setdiff(pids, dat1)
-  attr(pid_no_dat1, "message") <- "Sample with .pid file but not in Dat1_validated"
+  attr(pid_no_dat1, "message") <- "Sample with .pid file but no .txt file"
   
   dat1_no_pid <- setdiff(dat1, pids)
-  attr(dat1_no_pid, "message") <- "Sample in Dat1_validated but no .pid file"
+  attr(dat1_no_pid, "message") <- "Sample width .txt file but no .pid file"
 
   
   # message("\nCHECK OBJECTS")
   
   # message("Gather data")
   # read pid and dat1 data records
-  dpids <- ldply(pid_files, read_pid)
-  ddat1 <- ldply(dat1_files, read_pid)
+  dpids <- ldply(pid_files, read_pid, .inform=TRUE)
+  ddat1 <- ldply(dat1_files, read_pid, .inform=TRUE)
   
   # get image names from records
   dpids$img <- str_c(dpids$Label, "_", dpids$Item)
@@ -83,16 +87,16 @@ check_project_dispatcher.zooscan <- function(project) {
   
   # Check image presence
   pid_no_img <- setdiff(dpids$img, imgs)
-  attr(pid_no_img, "message") <- "Object in pids but no vignette"
+  attr(pid_no_img, "message") <- "Object referenced in .pid files but no vignette"
 
   dat1_no_img <- setdiff(ddat1$img, imgs)
-  attr(dat1_no_img, "message") <- "Object in Dat1_validated txt but no vignette"
+  attr(dat1_no_img, "message") <- "Object referenced in .txt file but no vignette"
   
   img_not_in_pid <- img_files[! imgs %in% dpids$img]
-  attr(img_not_in_pid, "message") <- "Vignette present but no object in pids"
+  attr(img_not_in_pid, "message") <- "Vignette present but no object in .pid files"
 
   img_not_in_dat1 <- img_files[! imgs %in% ddat1$img]
-  attr(img_not_in_dat1, "message") <- "Vignette present but no object in Dat1_validated txt"
+  attr(img_not_in_dat1, "message") <- "Vignette present but no object in .txt files"
   
 
   # Check identifications
@@ -114,7 +118,7 @@ check_project_dispatcher.zooscan <- function(project) {
   } else {
     wrong_ids <- character()
   }
-  attr(wrong_ids, "message") <- "Identification mismatch between Dat1_validated files and vignette location"
+  attr(wrong_ids, "message") <- "Identification mismatch between .txt files and vignette location"
   
   out <- list(
     in_meta_no_pid=in_meta_no_pid,

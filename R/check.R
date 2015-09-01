13a362d52613fe0dca0456cde1a568d957cf37aa
Jean-Olivier Irisson <irisson@normalesup.org>
2015-09-01 10:41:54 +0200

    Separate computation of problems and printing in project checking

--- R/check.R
+++ R/check.R
@@ -19,44 +19,25 @@
 #' }
 #'
 #' @return
-#' Returns a list with all problematic items (sample or object names), invisibly.
+#' Returns a list (of class \code{check}) containing all problematic items (sample or object names) that can be printed with various levels of detail (depending on \code{nmax})
 #'
 #' @export
-check <- function(project, nmax=50) {
+check <- function(project) {
   project <- project_class(project)
-  check_project(project=project, nmax)
+  check_project(project=project)
 }
 
-check_project <- function(project, nmax) {
+check_project <- function(project) {
   UseMethod("check_project")
 }
 
 #' @importFrom stringr str_c str_replace str_split
 #' @importFrom plyr ldply
-check_project.zooscan <- function(project, nmax) {
+check_project.zooscan <- function(project) {
 
-  # Helper function
-  alert <- function(x, message, nmax) {
-    # x is the vector of problematic items
-    # if it contains something, print a message and the list of problems, limited to nmax
-    n <- length(x)
-    if ( n > 0) {
-      message(message, " (",n ," elements)")
-      if ( n > nmax ) {
-        x <- x[1:nmax]
-      }
-      x <- str_c("  ", x, collapse="\n")
-      if ( n > nmax) {
-        x <- str_c(x, "\n  and ", (n - nmax), " others...")
-      }
-      message(x)
-    }
-  }
+  # message("CHECK SAMPLES")
 
-
-  message("CHECK SAMPLES")
-
-  message("Gather data")
+  # message("Gather data")
   # list all data referencing samples
   meta <- read_meta(project)
   pid_files <- list.files(str_c(project, "/Zooscan_scan/_work"), pattern=glob2rx("*.pid"), full=TRUE, recursive=TRUE)
@@ -67,27 +48,27 @@ check_project.zooscan <- function(project, nmax) {
   dat1 <- str_replace(basename(dat1_files), "_dat1.txt", "")
 
   in_meta_no_pid <- setdiff(meta$id, pids)
-  alert(in_meta_no_pid, "Sample in meta but no .pid file", nmax)
+  attr(in_meta_no_pid, "message") <- "Sample in meta but no .pid file"
   
   in_meta_no_dat1 <- setdiff(meta$id, dat1)
-  alert(in_meta_no_dat1, "Sample in meta but not in Dat1_validated", nmax)
+  attr(in_meta_no_dat1, "message") <- "Sample in meta but not in Dat1_validated"
 
   pid_not_in_meta <- setdiff(pids, meta$id)
-  alert(pid_not_in_meta, "Sample with .pid file but not in meta", nmax)
+  attr(pid_not_in_meta, "message") <- "Sample with .pid file but not in meta"
 
   dat1_not_in_meta <- setdiff(dat1, meta$id)
-  alert(dat1_not_in_meta, "Sample in Dat1_validated but not in meta", nmax)
+  attr(dat1_not_in_meta, "message") <- "Sample in Dat1_validated but not in meta"
   
   pid_no_dat1 <- setdiff(pids, dat1)
-  alert(pid_no_dat1, "Sample with .pid file but not in Dat1_validated", nmax)
+  attr(pid_no_dat1, "message") <- "Sample with .pid file but not in Dat1_validated"
   
   dat1_no_pid <- setdiff(dat1, pids)
-  alert(dat1_no_pid, "Sample in Dat1_validated but no .pid file", nmax)
+  attr(dat1_no_pid, "message") <- "Sample in Dat1_validated but no .pid file"
 
   
-  message("\nCHECK OBJECTS")
+  # message("\nCHECK OBJECTS")
   
-  message("Gather data")
+  # message("Gather data")
   # read pid and dat1 data records
   dpids <- ldply(pid_files, read_pid)
   ddat1 <- ldply(dat1_files, read_pid)
@@ -102,16 +83,16 @@ check_project.zooscan <- function(project, nmax) {
   
   # Check image presence
   pid_no_img <- setdiff(dpids$img, imgs)
-  alert(pid_no_img, "Object in pids but no vignette", nmax)
+  attr(pid_no_img, "message") <- "Object in pids but no vignette"
 
   dat1_no_img <- setdiff(ddat1$img, imgs)
-  alert(dat1_no_img, "Object in Dat1_validated txt but no vignette", nmax)
+  attr(dat1_no_img, "message") <- "Object in Dat1_validated txt but no vignette"
   
   img_not_in_pid <- img_files[! imgs %in% dpids$img]
-  alert(img_not_in_pid, "Vignette present but no object in pids", nmax)
+  attr(img_not_in_pid, "message") <- "Vignette present but no object in pids"
 
   img_not_in_dat1 <- img_files[! imgs %in% ddat1$img]
-  alert(img_not_in_dat1, "Vignette present but no object in Dat1_validated txt ", nmax)
+  attr(img_not_in_dat1, "message") <- "Vignette present but no object in Dat1_validated txt"
   
 
   # Check identifications
@@ -129,10 +110,47 @@ check_project.zooscan <- function(project, nmax) {
   # detect problems and report them
   wrong_ids <- ids[ids$from_dat1 != ids$from_img,]
   if ( nrow(wrong_ids) > 0) {
-    message <- str_c(wrong_ids$object, " should be ", wrong_ids$from_dat1, " but vignette is in ", wrong_ids$from_img)
-    alert(message, "Identification mismatch between Dat1_validated files and vignette location", nmax)
-    
+    wrong_ids <- str_c(wrong_ids$object, " should be ", wrong_ids$from_dat1, " but vignette is in ", wrong_ids$from_img)
+  } else {
+    wrong_ids <- character()
   }
+  attr(wrong_ids, "message") <- "Identification mismatch between Dat1_validated files and vignette location"
   
-  return(invisible(list(in_meta_no_pid, in_meta_no_dat1, pid_not_in_meta, dat1_not_in_meta, pid_no_dat1, dat1_no_pid, pid_no_img, dat1_no_img, img_not_in_pid, img_not_in_dat1, wrong_ids)))
+  out <- list(
+    in_meta_no_pid=in_meta_no_pid,
+    in_meta_no_dat1=in_meta_no_dat1,
+    pid_not_in_meta=pid_not_in_meta,
+    dat1_not_in_meta=dat1_not_in_meta,
+    pid_no_dat1=pid_no_dat1,
+    dat1_no_pid=dat1_no_pid,
+    pid_no_img=pid_no_img,
+    dat1_no_img=dat1_no_img,
+    img_not_in_pid=img_not_in_pid,
+    img_not_in_dat1=img_not_in_dat1,
+    wrong_ids=wrong_ids
+  )
+  class(out) <- c("check", class(out))
+  return(out)
+}
+
+#' @importFrom plyr l_ply
+#' @export
+#' @rdname check
+print.check <- function(x, nmax=50) {
+  l_ply(x, function(X) {
+    n <- length(X)
+    if (n > 0) {
+      title <- attr(X, "message")
+      message("\n", title, " (",n ," elements)")
+      if (n > nmax) {
+        X <- X[1:nmax]
+      }
+      X <- str_c("  ", X, collapse="\n")
+      if ( n > nmax) {
+       X <- str_c(X, "\n  and ", (n - nmax), " others...")
+      }
+     message(X)
+    }
+  })
+  return(invisible(x))
 }
